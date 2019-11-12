d4b2626fe1a3352ad5f3833dba59c34745b44b9d
Rebel <alles.rebel@wiliot.com>
2019-11-11 21:22:29 -0800

    basic icon stuff works, and we have a in memory db

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from flask.helpers import *
+from flask.app import *
 
 app = Flask(__name__)
+
+# We'll keep things super simple.
+# Everything will be inmemory for now
+
+"""
+Database connection setup etc
+"""
+
+app.users = {}
 
 """
 App Router Definitions + Blank Pages
 """
 
-@app.route( rule = '/', endpoint = 'index' )
+@app.route( rule = '/' )
 def index( **req ):
     print( req )
     return 'hello world'
@@ -14,5 +25,12 @@ def index( **req ):
 @app.route( rule = '/<string:user>', methods = ['GET', 'POST'] )
 @app.route( rule = '/settings/<string:user>', methods = ['GET', 'POST'] )
 def user( **req ):
+    #Handle Icon requests
+    if req['user'] in ['favicon.ico']:
+        return app.send_static_file( req['user'] )
+
     print( req )
+    if req['user'] not in app.users:
+        raise default_exceptions[404]
     return 'user page'
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Quick and Dirty Flask App
+Web App that does basic user confirmations with magic link
+and allows users to sign up.
+
+## Key Features
+* Allows users to register and verify themselves with magic link
+* Allows users to email other uses to create accounts
+* Allows users to suscribe to other users
+* Allows users to create content to send out to subscribers
